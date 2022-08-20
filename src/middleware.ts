b92e8a74c82395fcbf151e09a4b7dfc25b2789c6
myselfmohdsameer <msamerr57@gmail.com>
2022-08-21 01:11:21 +0530

    Updated middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,7 +3,7 @@ import { NextResponse } from "next/server";
 
 export async function middleware(req: NextRequest, ev: NextFetchEvent) {
   const slug = req.nextUrl.pathname.split("/").pop();
-  if (slug != "shorten") {
+  if (slug != "shorten" && slug!="favicon.ico" && slug!="manifest.json") {
     const slugFetch = await fetch(`${req.nextUrl.origin}/api/get-url/${slug}`);
     if (slugFetch.status === 404) {
       return NextResponse.redirect(req.nextUrl.origin);
